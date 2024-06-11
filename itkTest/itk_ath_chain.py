d9f832edb01463360eeafd5a81e4afc626cca379
pbutti <pierfrancesco.butti@gmail.com>
2024-06-11 12:34:30 +0200

    Added pixel seeding chain

--- itkTest/itk_ath_chain.py
+++ itkTest/itk_ath_chain.py
@@ -9,8 +9,15 @@ import acts.examples, acts.examples.itk
 
 from acts.examples import (
     CsvTrackingGeometryWriter,
-    RootAthInputReader
+    RootAthInputReader,
+    TrackParamsEstimationAlgorithm,
 )
+
+from acts.examples.reconstruction import (
+    addStandardSeeding,
+)
+
+from acts.examples.itk import InputSpacePointsType
 
 geo_dir = pathlib.Path("/eos/home-p/pibutti/acts-itk")
 detector, trackingGeometry, decorators = acts.examples.itk.buildITkGeometry(geo_dir)
@@ -22,9 +29,8 @@ if "__main__" == __name__:
     
     outputDir = "./"
 
-
     s = acts.examples.Sequencer(
-        events=1,
+        events=10,
         numThreads=1,
         outputDir = str(outputDir)
         )
@@ -32,12 +38,50 @@ if "__main__" == __name__:
     # Read Athena input space points and clusters from root file
 
     athReader = RootAthInputReader(
-        level=acts.logging.DEBUG,
+        level=acts.logging.INFO,
         treename  = "GNN4ITk",
-        inputfile = "/afs/cern.ch/user/p/pibutti/sw/gnn/Dump_GNN4Itk.root",
+        inputfile = "/eos/home-p/pibutti/sw/run/gnn/Dump_GNN4Itk.root",
     )
     
     s.addReader(athReader)
+
+    spacePoints = athReader.config.outputPixelSpacePoints
+    
+     # run seeding on Pixel Space points
+    inputPixelSeeds = addStandardSeeding(
+        s,
+        spacePoints,
+        *acts.examples.itk.itkSeedingAlgConfig(
+            InputSpacePointsType.PixelSpacePoints
+            ),
+        logLevel = acts.logging.DEBUG,
+        )
+
+
+    # run seeding on Strip Space Points
+    
+
+    # run seed to prototracks
+
+    prototracks = "seed-prototracks"
+    s.addAlgorithm(
+        acts.examples.SeedsToPrototracks(
+            level=acts.logging.DEBUG,
+            inputSeeds=inputSeeds,
+            outputProtoTracks=prototracks,
+            )
+        )
+    
+    # estimate seeding performance
+    parEstimateAlg = TrackParamsEstimationAlgorithm(
+        level=acts.logging.DEBUG,
+        inputSeeds=inputSeeds,
+        outputTrackParameters="estimatedparameters",
+        trackingGeometry=trackingGeometry,
+        magneticField=field,
+        )
+    
+    #s.addAlgorithm(parEstimateAlg)
 
 
     s.run()
